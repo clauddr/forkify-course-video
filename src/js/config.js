05a06e6253890e39559a2b737a63cf65162e9b05
clauddr <cla_udiu2006@yahoo.com>
2023-01-26 19:50:08 +0000

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9de0b763-eaf4-47c8-8bb0-b0fe33ab021e';
+export const MODAL_CLOSE_SEC = 2.5;
