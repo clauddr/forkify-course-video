415ba118c2226bcc8a141cd289fc36f30600362e
clauddr <cla_udiu2006@yahoo.com>
2023-01-27 07:28:12 +0000

    Removed Fractional

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-import { Fraction } from 'fractional';
+// import { Fraction } from 'fractional';
 import View from './View.js';
 
 class RecipeView extends View {
@@ -131,7 +131,8 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ''
+        // ing.quantity ? new Fraction(ing.quantity).toString() : ''
+        ing.quantity ? ing.quantity.toString() : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
